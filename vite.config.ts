e4c318cb714f5ae2005c73d5d5326d42e9951c75
stud0709 <stud0709.github@gmail.com>
2026-02-05 16:27:21 +0100

    preparing github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,9 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig(({ mode, command }) => ({
+  base: command === 'serve' ? '/' : '/oms4web/',
+
   server: {
     host: "::",
     port: 8080,
